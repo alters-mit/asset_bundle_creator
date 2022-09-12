3bef2051d35c561da167e273a8cdca2332148717
Seth Alter <alters@mit.edu>
2022-09-12 11:53:10 -0400

    reverted

--- Assets/Scripts/Editor/MeshConverter.cs
+++ Assets/Scripts/Editor/MeshConverter.cs
@@ -92,7 +92,7 @@ public static class MeshConverter
         // Try to convert the file.
         else
         {
-            return Assimp(source, ".obj", out path);
+            return Assimp(source, ".fbx", out path);
         }
     }
 
